faaa3e0413e88a652d5c56d0402ff4071d2f4337
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-03-27 08:37:11 -0700

    refactoring the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {  } from 'react';
 
-class App extends Component {
-  
-  sum(a: number, b: number): number {
-    return a + b;
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-            {this.sum(2, 15)}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <h1>Rick and Morty</h1>
+      <p>Pick your favorite character.</p>
+    </>
+  )
+};
 
 export default App;
